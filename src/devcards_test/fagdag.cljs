e362ce64f2fac5d6e472f324f2d29cf2af4437ab
Stian Sivertsen <stian-sivertsen@hotmail.com>
2016-10-05 21:20:23 +0200

    Added robot-table example

--- src/devcards_test/fagdag.cljs
+++ src/devcards_test/fagdag.cljs
@@ -17,3 +17,39 @@
        [:div.btn.btn-success
         {:on-click #(reset! times (inc @times))}
         "+1"]])))
+
+
+(def data (r/atom [{:id 1 :name "Robot1" :color "red" :ipaddress "123" :url "https://images-na.ssl-images-amazon.com/images/I/41IXITCtVwL._SY300_.jpg"}
+                   {:id 2 :name "Robot2" :color "blue" :ipaddress "123" :url "https://images-na.ssl-images-amazon.com/images/I/41IXITCtVwL._SY300_.jpg"}
+                   {:id 3 :name "Robot3" :color "green" :ipaddress "123" :url "https://images-na.ssl-images-amazon.com/images/I/41IXITCtVwL._SY300_.jpg"}]))
+
+(defn create-robot-row [{:keys [id name color ipaddress url] :as robot}]
+  ^{:key (str "robot-row-" id)}
+  [:tr
+    [:td id]
+    [:td name]
+    [:td 
+     {:style {:color color
+              :border (str "2px solid " color)}}
+     color]
+    [:td ipaddress]
+    [:td.text-center.img-responsive [:img {:src url :height 40}]]])
+
+(defn create-robot-table [data]
+  [:table.table.table-bordered
+       [:thead>tr
+        [:th "ID"]
+        [:th "Name"]
+        [:th "Color"]
+        [:th "IP"]
+        [:th "Image"]]
+      [:tbody
+        (map create-robot-row data)]])
+
+(defcard-rg robots-table
+  [:div.row
+    [:div.col-xs-12
+      [:h1 "Robots here!"]
+      [:hr]
+      (create-robot-table @data)]])
+      
